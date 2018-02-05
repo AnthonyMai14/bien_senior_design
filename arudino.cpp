1c913c0d525ba49b47caf260fbdcd68f4bf4ebe2
anthonymai14 <amai005@ucr.edu>
2018-02-05 05:54:37 +0000

    finish pseudo code

--- arudino.cpp
+++ arudino.cpp
@@ -1,62 +1,137 @@
 /*sample_obj
  * Purpose: To detect a signifianct change in position/angle
  * and return to detect_obj 
- * Variable: x1, x2, 
- *            y1, y2,
- *            z1, z2, (Position)
- *            gx1, gx2,
- *            gy1, gy2,
- *            gz1 gz2 (Gyroscope)
+ * Variable: x, y, z, (Position)
+ *            gx, gy, gz (Gyroscope)
  * Function: acceleration()
  * 
-  */
-
+ */
 //TODO: find out if need to include vector classs
-#import <vector.h>
-class Sample_obj{
-    private:
-      //put variable
-      double x1, y1, y2, z1;
-      double gx1, gy1, gz1;
-    public:
-      Sample_obj();// default constructor
-      Sample_obj(double x1, double y1, double z1,
-                double gx1, double gy1, double gz1);
+
+/*
+ * Base on the index, keep adding 
+ */
+ 
+ #include <QueueArray.h>
+
+void setup() {
+ 
+  /*
+   * Sample_obj constructor. When declaring and object of type Sample_obj
+   * You will use this struct to store the coordinate data that you get from
+   * the gyroscope
+  */
+  struct Sample_obj{
+    //put variable
+    double x, y, z;
+    double gx, gy, gz;
+    
   }
-Sample_obj::Sample_obj() {
-  this->x1 = 0;
-  this->x2 = 0;
-  this->y1 = 0;
-  this->y2 = 0;
-  this->z1 = 0; 
-  this->z2 = 0;
-  //TODO: finish
-  gx1, gx2, gy1, gy2, gz1 gz2
-}
-Sample_obj::Sample_obj(double x1, double x2, double y1, double y2, 
-                double z1, double z2, double gx1,double gx2,
-                double gy1,double gy2,double gz1,double gz2){
-  this->x1 = x1;
-  this->x2 = x2;
-  this->y1 = y1;
-  this->y2 = y2;
-  this->z1 = z1; 
-  this->z2 = z2;
-  //TODO: finish
-  gx1, gx2, gy1, gy2, gz1 gz2
+  
+  
+  /*
+   * Storage_obj: will store 2 Sample_obj and calculate the acceleration
+   * between the 2 Sample_oject
+   */
+  /*struct Storage_obj{
+    //sample_obj 
+    Sample_obj a;
+    Sample_obj b;
+    //acceleration vector
+    vector<double> acceleration(6);
+  
+    Storage_obj(Sample_obj, Sample_obj);    
+  }
+  Storage_obj::Storage_obj(Sample_obj a, Sample_obj b) {
+  
+    this->Sample_obj a = Sample_obj a;
+    this->Sample_obj b = Sample_obj b;
+    
+    acceleration.at(0) = b.x - a.x;
+    acceleration.at(1) = b.y - a.y;
+    acceleration.at(2) = b.z - a.z;
+    acceleration.at(3) = b.gx - a.gx;
+    acceleration.at(4) = b.gy - a.gy;
+    acceleration.at(5) = b.gz - a.gz;
+  }*/
+  double avg_acceleration(Sample_obj &a, Sample_obj &b) {
+    double acceleration[6];
+    
+    acceleration[0] = b.x - a.x;
+    acceleration[1] = b.y - a.y;
+    acceleration[2] = b.z - a.z;
+    acceleration[3] = b.gx - a.gx;
+    acceleration[4] = b.gy - a.gy;
+    acceleration[5] = b.gz - a.gz;
+    
+    return acceleration;
+  }
+  
+  struct Detect_obj{
+    const int VECTOR_WINDOW = 5; //the size of the window
+    const int COMPARE_SIZE = 4;
+    const int SIGNIFICANT_THRESHOLD = 3;
+    
+    double acceleration_array[VECTOR_WINDOW];
+    bool detect_significant();
+  }
+  bool Detect_obj::detect_significant() {
+    //TODO: algorithm
+    if (acceleration_array[VECTOR_WINDOW - 1] != NULL) {
+      int num_of_sign_change = 0;
+      for (int i = 0; i < VECTOR_WINDOW - 1; ++i) {
+        //calculate if pos or neg for both index
+        //compare if change in pos/neg
+          //if yes, increase num of sign change by one
+      }
+      if (num_of_sign_change >= SIGNIFICANT_THRESHOLD) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  Detect_obj main_obj;
+  QueueArray <Sample_obj> sample_obj_q;
+  index = 0;
 }
 
-std::vector<double> Sample_obj::acceleration() {
-  std::vector data(12);
-  data.at(0) = x2 - x1;
-  data.at(1) = y2 - y1;
-  data.at(2) = z2 - z1;
-  //todo till data.at(5)
-  return data;
-}
+void loop() {
+  //get x,y,z,gx,gy,gz data from Gyroscope
+  Sample_obj data;
+  
+  //store the data from Gyroscope in a Storage_obj and push into sample_obj_q
+  data.x = gyroscope.x;
+  data.y = gyroscope.y;
+  data.z = gyroscope.z;
+  data.gx = gyroscope.gx;
+  data.gy = gyroscope.gy;
+  data.gz = gyroscope.gz;
 
-class Detect_obj{
-  private:
-    //
-  public:
-}l
+  
+  sample_obj_q.push(data);
+  
+  if (sample_obj_q.count == 2) {
+    //take 2 value of Sample_obj
+    Sample_obj a = sample_obj_q.pop();
+    Sample_obj b = sample_obj_q.front();
+    
+    //TODO: calculate acceleration average
+    double acceleration_avg = 0;
+    
+    //add to Detect_obj
+    main_obj.acceleration_array[index];
+    if (index >= 5) {
+      index = 0;
+    }
+    else {
+      ++index;
+    }
+    
+     if (main_obj.detect_significant()) {
+        //send message
+        //slowdown time
+        //exit program OR call 911?
+     }
+  }
+}
